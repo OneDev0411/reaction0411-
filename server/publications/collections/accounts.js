OneDev0411/reaction0411-

commit f1029b4f218c0d9b53af09fff5d6b49507a7e385

Exclude anonymous users from accounts publication (#1390)
* Exclude anonymous users from accounts publication when logged in as admin

* NPM imports first

server/publications/collections/accounts.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import * as Collections from "/lib/collections";
 import { Reaction } from "/server/api";
 
@@ -16,12 +17,24 @@ Meteor.publish("Accounts", function (userId) {
   if (!shopId) {
     return this.ready();
   }
+
+  const nonAnonUsers = _.pluck(Meteor.users.find({
+    [`roles.${shopId}`]: {
+      $nin: [ "anonymous" ]
+    }
+  }, {
+    fields: { _id: 1 }
+  }).fetch(), "_id");
+
   // global admin can get all accounts
   if (Roles.userIsInRole(this.userId, ["owner"], Roles.GLOBAL_GROUP)) {
-    return Collections.Accounts.find();
-    // shop admin gets accounts for just this shop
+    return Collections.Accounts.find({
+      _id: { $in: nonAnonUsers }
+    });
+  // shop admin gets accounts for just this shop
   } else if (Roles.userIsInRole(this.userId, ["admin", "owner"], shopId)) {
     return Collections.Accounts.find({
+      _id: { $in: nonAnonUsers },
       shopId: shopId
     });
   }

commit 09dd97c89c93d2b5f33aac706012c362285e3de4

update deprecated _.pluck
- Patch _.pluck = _.map in lodash 4.x
- resolves issue introduced in #1390

server/publications/collections/accounts.js
@@ -18,7 +18,7 @@ Meteor.publish("Accounts", function (userId) {
     return this.ready();
   }
 
-  const nonAnonUsers = _.pluck(Meteor.users.find({
+  const nonAnonUsers = _.map(Meteor.users.find({
     [`roles.${shopId}`]: {
       $nin: [ "anonymous" ]
     }